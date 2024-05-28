a63c962d383fda2b18c6b22b387eabf5746ad147
Nam <namdeveloper.ca@gmail.com>
2024-05-28 16:50:17 +0700

    basic setup 101

---
+++ src/lib/utils.ts
@@ -0,0 +1,50 @@
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+import type { Metadata } from 'next'
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export function constructMetadata({
+  title = 'OSS Gallery',
+  description = 'A collection of open-source projects built with Dub.',
+  image = 'https://oss.gallery/thumbnail.jpg',
+}: {
+  title?: string
+  description?: string
+  image?: string | null
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      ...(image && {
+        images: [
+          {
+            url: image,
+          },
+        ],
+      }),
+    },
+    twitter: {
+      title,
+      description,
+      ...(image && {
+        card: 'summary_large_image',
+        images: [image],
+      }),
+      creator: '@dubdotco',
+    },
+    metadataBase: new URL('https://oss.gallery'),
+  }
+}
+
+export const getUrlWithRef = (url: string) => {
+  const urlWithRef = new URL(url)
+  urlWithRef.searchParams.set('ref', 'ossgallery')
+
+  return urlWithRef.toString()
+}
